9bc2f3504b5165331170d86293e9a690cadbff49
Dan Hipschman <dsh@vicarious.com>
2015-02-21 11:42:36 -0800

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Dan Hipschman on 2/21/15.
+//  Copyright (c) 2015 Dan Hipschman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Twitter
+//
+//  Created by Dan Hipschman on 2/21/15.
+//  Copyright (c) 2015 Dan Hipschman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
